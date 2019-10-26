83416f383b8f692699958b2f73d7d838a483445d
affonja <affonja@yandex.ru>
2019-10-27 01:06:02 +0300

    переименованы массивы

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 $is_auth = rand(0, 1);
 
-$category_arr = ['Доски и лыжи', 'Крепления', 'Ботинки', 'Одежда', 'Инструменты', 'Разное'];
-$lot_arr = [
+$categories = ['Доски и лыжи', 'Крепления', 'Ботинки', 'Одежда', 'Инструменты', 'Разное'];
+$lots = [
     [
         'name' => '2014 Rossignol District Snowboard',
         'category' => 'Доски и лыжи',
@@ -98,9 +98,9 @@ $user_name = 'Ольга'; // укажите здесь ваше имя
                 горнолыжное снаряжение.</p>
             <ul class="promo__list">
                 <!--заполните этот список из массива категорий-->
-                <?php foreach ($category_arr as $item) : ?>
+                <?php foreach ($categories as $category) : ?>
                     <li class="promo__item promo__item--boards">
-                        <a class="promo__link" href="pages/all-lots.html"><?= $item; ?></a>
+                        <a class="promo__link" href="pages/all-lots.html"><?= $category; ?></a>
                     </li>
                 <?php endforeach; ?>
             </ul>
@@ -111,19 +111,19 @@ $user_name = 'Ольга'; // укажите здесь ваше имя
             </div>
             <ul class="lots__list">
                 <!--заполните этот список из массива с товарами-->
-                <?php foreach ($lot_arr as $lot) : ?>
+                <?php foreach ($lots as $lot) : ?>
                     <li class="lots__item lot">
                         <div class="lot__image">
-                            <img src="<?= $lot[img]; ?>" width="350" height="260" alt="<?= $lot[name]; ?>">
+                            <img src="<?= $lot['img']; ?>" width="350" height="260" alt="<?= $lot['name']; ?>">
                         </div>
                         <div class="lot__info">
-                            <span class="lot__category"><?= $lot[category]; ?></span>
-                            <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?= $lot[name]; ?></a>
+                            <span class="lot__category"><?= $lot['category']; ?></span>
+                            <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?= $lot['name']; ?></a>
                             </h3>
                             <div class="lot__state">
                                 <div class="lot__rate">
                                     <span class="lot__amount">Стартовая цена</span>
-                                    <span class="lot__cost"><?= $lot[price]; ?><b class="rub">р</b></span>
+                                    <span class="lot__cost"><?= $lot['price']; ?><b class="rub">р</b></span>
                                 </div>
                                 <div class="lot__timer timer">
                                     12:23
@@ -141,9 +141,9 @@ $user_name = 'Ольга'; // укажите здесь ваше имя
     <nav class="nav">
         <ul class="nav__list container">
             <!--заполните этот список из массива категорий-->
-            <?php foreach ($category_arr as $item) : ?>
+            <?php foreach ($categories as $category) : ?>
                 <li class="nav__item">
-                    <a href="pages/all-lots.html"><?= $item; ?></a>
+                    <a href="pages/all-lots.html"><?= $category; ?></a>
                 </li>
             <?php endforeach; ?>
         </ul>
