72249e8f6e336d17d91c70fca0ea83a0f85b7cdf
affonja <affonja@yandex.ru>
2019-12-17 10:53:57 +0300

    исправлен init.php

--- init.php
+++ init.php
@@ -6,6 +6,9 @@ error_reporting(E_ALL);
 date_default_timezone_set("Europe/Moscow");
 session_start();
 
+if (!file_exists('config/config.php')) {
+    exit('Создайте файл config/config.php на основе config.sample.php и сконфигурируйте его');
+}
 $config = require 'config/config.php';
 require_once('functions/helpers.php');
 require_once('functions/getters.php');
